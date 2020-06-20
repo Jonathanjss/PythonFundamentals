9bdf8f5d793ec2ea13d3eeaac6b7f95a31ebd146
Jonathan <jjimenezssa@gmail.com>
2020-06-19 23:49:58 -0500

    Calculate areas homework finished.

--- homework/calculateAreas.py
+++ homework/calculateAreas.py
@@ -1,19 +1,77 @@
 import sys
+import math
+
 def calcSquareArea():
+    print('##### SQUARE AREA #####')
     side = float(input('Please enter the size of the square side: '))
     squareArea = side ** 2
-    print(f'Square area is {squareArea} m^2')
-    
+    print(f'\nSquare area is {squareArea} m^2')
+    print('\n\nWhat would you like to do?\n\t1) Calculate area again.\n\t2) Calculate area of another figure.\n\t3) Exit.')
+    functionState = 'square'
+    nextAction(functionState)
+
+def calcRentangleArea():
+    print('##### RECTANGLE AREA #####')
+    length = float(input('Please enter the length of the rectangle: '))
+    width = float(input('Please enter the width of the rectangle: '))
+    rectangleArea = length * width
+    print(f'\nRectangle area is {rectangleArea} m^2')
+    print('\n\nWhat would you like to do?\n\t1) Calculate area again.\n\t2) Calculate area of another figure.\n\t3) Exit.')
+    functionState = 'rectangle'
+    nextAction(functionState)
+
+def calcTriangleArea():
+    print('##### TRIANGLE AREA #####')
+    length = float(input('Please enter the length of the rectangle: '))
+    base = float(input('Please enter the width of the rectangle: '))
+    triangleArea = (length * base) / 2
+    print(f'\nTriangle area is {triangleArea} m^2')
+    print('\n\nWhat would you like to do?\n\t1) Calculate area again.\n\t2) Calculate area of another figure.\n\t3) Exit.')
+    functionState = 'triangle'
+    nextAction(functionState)
+
+def calcCircleArea():
+    print('##### CIRCLE AREA #####')
+    radius = float(input('Please enter the circle radius: '))
+    circleArea = math.pi * (math.pow(radius, 2))
+    print(f'\nCircle area is {circleArea} m^2')
+    print('\n\nWhat would you like to do?\n\t1) Calculate area again.\n\t2) Calculate area of another figure.\n\t3) Exit.')
+    functionState = 'circle'
+    nextAction(functionState)
+
+def nextAction(functionState):
+    userSelection = int(input('Please choose an option: '))
+    afterCalcArea(userSelection, functionState)
+
+def afterCalcArea(userSelection,functionState):
+    if userSelection == 1:
+        if functionState == 'square':
+            calcSquareArea()
+        elif functionState == 'rectangle':
+            calcRentangleArea()
+        elif functionState == 'triangle':
+            calcTriangleArea()
+        elif functionState == 'circle':
+            calcCircleArea()
+    elif userSelection == 2:
+        initApp()
+    elif userSelection == 3:
+        print("Bye")
+        sys.exit()
+    else:
+        while True:
+            userSelection = int(input('Non-valid option. Please choose a valid option: '))
+            afterCalcArea(userSelection, functionState)
 
 def menuApp(userSelection):
     if userSelection == 1:
         calcSquareArea()
     elif userSelection == 2:
-        print('rectangle')
+        calcRentangleArea()
     elif userSelection == 3:
-        print('triangle')
+        calcTriangleArea()
     elif userSelection == 4:
-        print('circle')
+        calcCircleArea()
     elif userSelection == 5:
         print('Bye')
         sys.exit()
